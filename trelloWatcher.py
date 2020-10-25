7d1b3fd805c4b3dbe42f81bcc3342f6d4b2aba5e
Ana Calabano <acalabano@circleblack.com>
2020-10-25 08:59:02 -0400

    trello get the cache

--- trelloWatcher.py
+++ trelloWatcher.py
@@ -130,7 +130,10 @@ async def update_trello_cards_and_time():
                                         num_codes_needed = find_number_of_codes_needed(card)
                                         append_random_codes(card, num_codes_needed)
                                     await client.wait_until_ready()
-                                    code_giver = client.get_user(jiselConf['trello']['trello_discord_id_pair'][card_action['memberCreator']['username']])
+                                    code_giver = client.get_user(int(jiselConf['trello']['trello_discord_id_pair'][card_action['memberCreator']['username']]))
+                                    print('jisel---- none prob')
+                                    print(jiselConf['trello']['trello_discord_id_pair'])
+
                                     embed = Embed(title=f"You have sent {hoster_receiving_codes} the following codes:", description=card.description, color=0x00ff00)
                                     await code_giver.send(embed=embed)
                                     hoster_embed = Embed(title=f"{code_giver.name} has prepared codes for your request:", description=card.description, color=0x00ff00)
