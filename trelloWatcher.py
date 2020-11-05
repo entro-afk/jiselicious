65b1200693f331d91a61ca229dc14a961b8c294b
Ana Calabano <acalabano@circleblack.com>
2020-11-05 12:16:42 -0500

    feedback on watcher

--- trelloWatcher.py
+++ trelloWatcher.py
@@ -187,7 +187,7 @@ async def update_trello_cards_and_time():
             print('The random minute------', random_minute)
             last_hour = r.get('lasthour')
             has_started_trivia = r.get('start')
-            print('has_started_trivia-------------', has_started_trivia)
+            print('has_started_trivia-------------', str(has_started_trivia))
             print('The last hour------', last_hour)
             if last_hour and str(has_started_trivia) == 'yes':
                 last_hour = int(last_hour)
